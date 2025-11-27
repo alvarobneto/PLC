3996eeb902b83b8d48ff336bc43c27118d27b938
Álvaro <145587864+alvarobneto@users.noreply.github.com>
2025-11-27 00:42:29 -0300

    Add Poupanca class extending Conta

---
+++ java/aula09/Poupanca.java
@@ -0,0 +1,44 @@
+public class Conta{
+    private String numero;
+    private double saldo;
+
+    public Conta(String numero, double saldo){
+        this.numero = Conta.getProximo();
+        this.saldo = saldo;
+    }
+
+    public void creditar(double valor){
+        saldo = saldo + valor;
+    }
+    //EX: Conta c = new Conta;
+    //c.creditar(3.30);
+
+    public void debitar(double valor){
+        saldo = saldo - valor;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public double getNum(){
+        return numero;
+    }
+
+    private static int getProximo(){
+        proximo = proximo + 1;
+        return proximo;
+    }
+}
+
+public class Poupanca extends Conta {
+
+    public Poupanca(String numero){
+        super(numero);
+    }
+
+    public void renderJuros(double taxa){
+        double juros = this.getSaldo() * taxa;
+        this.creditar(juros); 
+     }
+}
